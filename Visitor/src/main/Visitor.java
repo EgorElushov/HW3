7915c700c8d33ece323c4314a44277f830561c26
Egor Elushov <evelushov@yandex-team.ru>
2023-03-27 03:12:02 +0300

    add subprojects to gradle

---
+++ Visitor/src/main/Visitor.java
@@ -0,0 +1,13 @@
+package main;
+
+public class Visitor {
+    private int value;
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+}
